5ac88d51e49bfffd8cb5a70287a7ea45a6837256
Wojtek Mitan <wojtekmitan91@gmail.com>
2024-01-30 20:25:47 +0100

    MONGODB connected

--- app/TicketPage/[id]/page.jsx
+++ app/TicketPage/[id]/page.jsx
@@ -1,4 +1,6 @@
+import TicketForm from "@/app/(components)/TicketForm";
+
 const TicketPage = ({ params }) => {
-  return <div>TicketPage ${params.id}</div>;
+  return <TicketForm />;
 };
 export default TicketPage;
